a10d39cd78bec4cc5655f68910e3c1a7afe9204c
Subhash Kumar Singh <skumarsingh@synamedia.com>
2026-03-18 12:24:23 +0530

    update the user data

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-set -e
 
-USERNAME="${username}"
-PUBLIC_KEY="${public_key}"
+useradd -m -s /bin/bash ${username}
+mkdir -p /home/${username}/.ssh
 
-echo "Creating user..."
-useradd -m -s /bin/bash $USERNAME
+echo "${public_key}" > /home/${username}/.ssh/authorized_keys
 
-echo "Setting up SSH..."
-mkdir -p /home/$USERNAME/.ssh
-echo "$PUBLIC_KEY" > /home/$USERNAME/.ssh/authorized_keys
+chmod 700 /home/${username}/.ssh
+chmod 600 /home/${username}/.ssh/authorized_keys
 
-chmod 700 /home/$USERNAME/.ssh
-chmod 600 /home/$USERNAME/.ssh/authorized_keys
-chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
+chown -R ${username}:${username} /home/${username}/.ssh
 
-echo "Disabling password authentication..."
-sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-systemctl restart sshd
-
-echo "Setup complete"
+echo "${username} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${username}
+chmod 440 /etc/sudoers.d/${username}
